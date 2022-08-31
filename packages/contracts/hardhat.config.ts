50c89ce184a063252169acd0acb640cf0c91daec
KentaKudo <yd37574@gmail.com>
2022-08-31 22:22:19 +0100

    calculate gas price

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,6 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
+import dotenv from "dotenv";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.16",
@@ -8,6 +12,11 @@ const config: HardhatUserConfig = {
     hardhat: {
       chainId: 1337,
     },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "GBP",
+    coinmarketcap: process.env.COIN_MARKET_CAP_API_KEY,
   },
 };
 
